7579e29970f2f121574c4156fe339c5b551eb1ba
vedantc <you@example.comc.vedant18@gmail.com>
2020-10-21 23:43:17 +0530

    Add register page

--- our_project/user/views.py
+++ our_project/user/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from django.views.generic import TemplateView,CreateView,FormView
+from .forms import MemberForm
 # Create your views here.
+
+class Index(TemplateView):
+    template_name='index.html'
+
+class Register(CreateView):
+    form_class=MemberForm
+    template_name='register.html'
+    success_url='index'
